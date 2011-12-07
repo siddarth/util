d1e09805818feaeace3378d2a026f596f8d7f494
Siddarth Chandrasekaran <chandrasekaran.siddarth@gmail.com>
2011-12-06 19:52:34 -0500

    try/catch ratelimits so that we can still see stats.

--- twitter-response-time/calculate_times.rb
+++ twitter-response-time/calculate_times.rb
@@ -9,20 +9,29 @@ user = ARGV[0]
 total_time = 0.0
 num_tweets = 0
 
+def print_stats(rep_time_total, n)
+  str = "[info] Average response time = %f " % (rep_time_total / n )
+  str += "after %d tweets\n" % n
+  print str
+end
+
 (1..num_pages).each { |page_num|
-  timeline = Twitter.user_timeline(user, { "page" => page_num })
-  timeline.each { |tweet|
-    next if tweet.in_reply_to_status_id.nil?
-    #puts tweet.text
-    response_time = Time.parse(tweet.created_at)
-    in_reply_to = Twitter.status(tweet.in_reply_to_status_id)
-    original_time = Time.parse(in_reply_to.created_at)
-    num_tweets += 1
-    total_time += (response_time - original_time)
-    if (num_tweets % 100 == 0)
-      str = "[info] Average response time = %f " % (total_time / num_tweets )
-      str += "after %d tweets\n" % num_tweets
-      print str
-    end
-  }
+  begin
+    timeline = Twitter.user_timeline(user, { "page" => page_num })
+    timeline.each { |tweet|
+      next if tweet.in_reply_to_status_id.nil?
+      #puts tweet.text
+      response_time = Time.parse(tweet.created_at)
+      in_reply_to = Twitter.status(tweet.in_reply_to_status_id)
+      original_time = Time.parse(in_reply_to.created_at)
+      num_tweets += 1
+      total_time += (response_time - original_time)
+      if (num_tweets % 100 == 0)
+      end
+    }
+  rescue Exception => e
+    puts "[error] Rate-limited. %s" % e.message
+    print_stats(total_time, num_tweets)
+    exit(-1)
+  end
 }
